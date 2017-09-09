2e257ab5f3dccce4f671030706c77f9374e5aae0
Daniel Diaz <danieldiaz3000@gmail.com>
2017-09-09 15:41:59 -0500

    Cambio de nombre

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
       			<a href="#" class="brand-logo">Logo</a>
       			<ul id="nav-mobile" class="right hide-on-med-and-down">
 		        		<li><a href="sass.html">Prueba 1</a></li>
-		        		<li><a href="badges.html">Components</a></li>
+		        		<li><a href="badges.html">Cmabio</a></li>
 		        		<li><a href="collapsible.html">JavaScript</a></li>
 	      		</ul>
 		</div>
