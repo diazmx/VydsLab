cd809512194740dfd290564339bfde5f21d38de0
Daniel <danieldiaz3000@gmail.com>
2017-09-09 15:37:59 -0500

    Prueba

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script type="js/jquery-3.2.1.min.js"></script>
+	<script type="js/materialize.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+</head>
+<body>
+	<nav>
+    		<div class="nav-wrapper">
+      			<a href="#" class="brand-logo">Logo</a>
+      			<ul id="nav-mobile" class="right hide-on-med-and-down">
+		        		<li><a href="sass.html">Sass</a></li>
+		        		<li><a href="badges.html">Components</a></li>
+		        		<li><a href="collapsible.html">JavaScript</a></li>
+	      		</ul>
+		</div>
+	</nav>
+</body>
+</html>
